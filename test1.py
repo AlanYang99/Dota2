ee2ba150b430f860fc8dba4b52c64745516262bc
Urthor <hamishgcp@gmail.com>
2018-07-06 15:31:59 +1000

    Update test1.py

--- test1.py
+++ test1.py
@@ -260,6 +260,6 @@ def back():
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port, debut=True)
+    app.run(host='0.0.0.0', port=port, debug=True)
 #print (my_form_post())
 
